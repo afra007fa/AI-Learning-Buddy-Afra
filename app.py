e0280234239f580f0b584e2e0ccb4d924e06a36d
FATHIMA AFRA M J <afrajamaludeen2007@gmail.com>
2026-07-02 22:52:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 import os
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 st.set_page_config(page_title="AI Learning Buddy Afra", page_icon="🎓")
 
